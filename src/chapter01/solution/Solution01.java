3cba6b8e525a52257dfaba0ce6f3f2d931a332e1
qxq <812573459@qq.com>
2021-07-27 14:43:20 +0800

    Initial commit

---
+++ src/chapter01/solution/Solution01.java
@@ -0,0 +1,14 @@
+package chapter01.solution;
+
+/**
+ * @author qxq
+ * @date 2021/7/27
+ */
+public class Solution01 {
+    static int anInt;
+    static char aChar;
+
+    public static void main(String[] args) {
+        System.out.println("int = " + anInt + "\nchar = " + aChar);
+    }
+}
